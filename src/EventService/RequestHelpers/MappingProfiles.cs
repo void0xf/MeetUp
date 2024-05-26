a7e487c604cb9e8defb5526940d3882af9951a65
void0xf <lucyfer99100@gmail.com>
2024-05-26 20:03:33 +0200

    feat: add automapper for MeetEvent

---
+++ src/EventService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EventService.DTOs;
+using EventService.Models;
+
+namespace EventService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<MeetEvent, MeetEventDto>();
+        CreateMap<CreateMeetEventDto, MeetEvent>();
+    }
+}
