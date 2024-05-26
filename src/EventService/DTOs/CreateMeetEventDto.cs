40cd132d1b88badd6a8fa473199b56ff65388485
void0xf <lucyfer99100@gmail.com>
2024-05-26 17:41:48 +0200

    feat: added baisc DTOs

---
+++ src/EventService/DTOs/CreateMeetEventDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventService.DTOs;
+
+public class CreateMeetEventDto
+{
+    [Required]
+    public string Title { get; set; }
+
+    [Required]
+    public string? Description { get; set; }
+
+    [Required]
+    public DateTime EventStartDate { get; set; }
+
+    [Required]
+    public DateTime EventEndDate { get; set; }
+
+    [Required]
+    public string Location { get; set; }
+
+    [Required]
+    public string Author { get; set; }
+
+    [Required]
+    public string Visibility { get; set; }
+
+    [Required]
+    public List<string> Images { get; set; }
+}
---
+++ src/EventService/DTOs/MeetEventDto.cs
@@ -0,0 +1,15 @@
+﻿namespace EventService.DTOs;
+
+public class MeetEventDto
+{
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime EventStartDate { get; set; }
+    public DateTime EventEndDate { get; set; }
+    public string Location { get; set; }
+    public string Author { get; set; }
+    public string Visibility { get; set; }
+    public List<string> Images { get; set; }
+}
