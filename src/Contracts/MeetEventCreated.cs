88ba6f3aea962df1ca740624281d40b4b4f990de
void0xf <lucyfer99100@gmail.com>
2024-05-27 18:52:07 +0200

    feat: add contracts for data object consistency

---
+++ src/Contracts/MeetEventCreated.cs
@@ -0,0 +1,16 @@
+﻿namespace Contracts;
+
+public class MeetEventCreated
+{
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime EventStartDate { get; set; }
+
+    public DateTime EventEndDate { get; set; }
+    public string Location { get; set; }
+    public string Author { get; set; }
+    public string Visibility { get; set; }
+    public List<string> Images { get; set; }
+}
---
+++ src/Contracts/MeetEventDeleted.cs
@@ -0,0 +1,6 @@
+﻿namespace Contracts;
+
+public class MeetEventDeleted
+{
+    public string Id { get; set; }
+}
