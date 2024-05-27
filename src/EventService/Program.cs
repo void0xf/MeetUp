937af3e8b80876b47a764eb9d925e0c9f2aeaa81
void0xf <lucyfer99100@gmail.com>
2024-05-27 18:50:24 +0200

    feat: add rabitmq, MassTrasit

--- src/EventService/Program.cs
+++ src/EventService/Program.cs
@@ -1,4 +1,5 @@
 using EventService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,15 @@ builder.Services.AddDbContext<MeetEventDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq(
+        (context, cfg) =>
+        {
+            cfg.ConfigureEndpoints(context);
+        }
+    );
+});
 
 var app = builder.Build();
 
