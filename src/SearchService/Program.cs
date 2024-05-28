08aab3214519a5115089d6bd3536bcfffba0e264
void0xf <lucyfer99100@gmail.com>
2024-05-28 13:31:58 +0200

    feat: add Message retry policy

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -15,6 +15,14 @@ builder.Services.AddMassTransit(x =>
     x.UsingRabbitMq(
         (context, cfg) =>
         {
+            cfg.ReceiveEndpoint(
+                "search-meet-event-created",
+                e =>
+                {
+                    e.UseMessageRetry(r => r.Interval(5, 5));
+                    e.ConfigureConsumer<MeetEventCreatedConsumer>(context);
+                }
+            );
             cfg.ConfigureEndpoints(context);
         }
     );
