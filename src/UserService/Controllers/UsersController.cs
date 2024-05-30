ff966fe20f12f10fee07fdc7fed16a6c9a3a58a8
void0xf <lucyfer99100@gmail.com>
2024-05-30 14:52:25 +0200

    feat(User Services): add Controller for CRUD with authorization

---
+++ src/UserService/Controllers/UsersController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using UserService.Models;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<UserInfo>> Create(UserInfo userInfo)
+        {
+            var existingUserInfo = await DB.Find<UserInfo>()
+                .Match(u => u.Username == userInfo.Username)
+                .ExecuteFirstAsync();
+            if (existingUserInfo != null)
+            {
+                return BadRequest("User already exists");
+            }
+
+            await userInfo.SaveAsync();
+            return CreatedAtAction(
+                nameof(GetByUsername),
+                new { username = userInfo.Username },
+                userInfo
+            );
+        }
+
+        [HttpGet("{username}")]
+        public async Task<ActionResult<UserInfo>> GetByUsername(string username)
+        {
+            var userInfo = await DB.Find<UserInfo>()
+                .Match(u => u.Username == username)
+                .ExecuteFirstAsync();
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+            return Ok(userInfo);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UserInfo>>> GetAll()
+        {
+            var userInfos = await DB.Find<UserInfo>().ExecuteAsync();
+            return Ok(userInfos);
+        }
+
+        [Authorize]
+        [HttpPut("{username}")]
+        public async Task<IActionResult> Update(string username, UserInfo updatedUserInfo)
+        {
+            var userInfo = await DB.Find<UserInfo>()
+                .Match(u => u.Username == username)
+                .ExecuteFirstAsync();
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            if (userInfo.Username != User.Identity.Name)
+            {
+                return Forbid();
+            }
+
+            userInfo.Description = updatedUserInfo.Description;
+            userInfo.Fullname = updatedUserInfo.Fullname;
+            userInfo.WhoCanMessage = updatedUserInfo.WhoCanMessage;
+            userInfo.ChatsId = updatedUserInfo.ChatsId;
+
+            await userInfo.SaveAsync();
+            return NoContent();
+        }
+
+        [Authorize]
+        [HttpDelete("{username}")]
+        public async Task<IActionResult> Delete(string username)
+        {
+            var userInfo = await DB.Find<UserInfo>()
+                .Match(u => u.Username == username)
+                .ExecuteFirstAsync();
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            if (userInfo.Username != User.Identity.Name)
+            {
+                return Forbid();
+            }
+
+            await userInfo.DeleteAsync();
+            return NoContent();
+        }
+    }
+}
