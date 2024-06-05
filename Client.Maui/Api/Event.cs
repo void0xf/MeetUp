4a2d50cd1b66a67177cb6687f2e1238bfe7c2d1c
void0xf <lucyfer99100@gmail.com>
2024-06-05 10:17:58 +0200

    refactor: update event class

--- Client.Maui/Api/Event.cs
+++ Client.Maui/Api/Event.cs
@@ -2,16 +2,17 @@
 {
     public class Event
     {
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime EventStartDate { get; set; }
         public DateTime EventEndDate { get; set; }
         public string Location { get; set; }
         public string Author { get; set; }
         public string Visibility { get; set; }
+        public List<string> Participants { get; set; }
+        public string ConversationId { get; set; }
         public List<string> Images { get; set; }
-        public string Id { get; set; }
     }
 }
