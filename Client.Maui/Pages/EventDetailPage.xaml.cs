c6b64f0b8dd17e588177711b69d9ee68fafffbc8
void0xf <lucyfer99100@gmail.com>
2024-08-15 19:40:06 +0200

    feat: implement event detail page

--- Client.Maui/Pages/EventDetailPage.xaml.cs
+++ Client.Maui/Pages/EventDetailPage.xaml.cs
@@ -1,12 +1,13 @@
+using Client.Maui.Api.MeetEvent;
 using Client.Maui.ViewModels;
 
 namespace Client.Maui.Pages;
 
 public partial class EventDetailPage : ContentPage
 {
-    public EventDetailPage()
+    public EventDetailPage(IMeetEventApi meetEventApi)
     {
         InitializeComponent();
-        BindingContext = new EventDetailViewModel();
+        BindingContext = new EventDetailViewModel(meetEventApi);
     }
 }
--- Client.Maui/ViewModels/EventDetailViewModel.cs
+++ Client.Maui/ViewModels/EventDetailViewModel.cs
@@ -1,11 +1,30 @@
-﻿using Client.Maui.Api.Search;
+﻿using Client.Maui.Api;
+using Client.Maui.Api.MeetEvent;
+using Client.Maui.Store;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Client.Maui.ViewModels;
 
 [QueryProperty(nameof(Event), "Event")]
 public partial class EventDetailViewModel : ObservableObject
 {
+    private readonly IMeetEventApi _meetEventApi;
+    private readonly UserStore _userStore;
+
+    public EventDetailViewModel(IMeetEventApi meetEventApi)
+    {
+        _meetEventApi = meetEventApi;
+        _userStore = App.Current.Handler.MauiContext.Services.GetService<UserStore>();
+    }
+
     [ObservableProperty]
-    private SearchResponse.Event _event;
+    private Event _event;
+
+    [RelayCommand]
+    async void JoinEvent()
+    {
+        var res = await _meetEventApi.JoinToEvent(Event.Id.ToString(), _userStore.AccessToken);
+        if (res.StatusCode == System.Net.HttpStatusCode.OK) { }
+    }
 }
